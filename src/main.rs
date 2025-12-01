04dc1a6344852cd32ca454476117f88a24e9d498
Christian Legge <christian@christianlegge.dev>
2025-12-01 12:18:23 -0500

    rework main

--- src/main.rs
+++ src/main.rs
@@ -2,34 +2,45 @@ mod puzzles;
 
 use seq_macro::seq;
 
-#[allow(clippy::cognitive_complexity)]
-fn main() {
+fn get_puzzle_arg() -> Option<String> {
     let args = std::env::args().collect::<Vec<String>>();
-    if args.len() == 1 {
-        println!("No arguments provided");
+    if args.len() == 2 {
+        Some(args[1].to_string())
     } else {
-        let arg = args[1].as_str();
-        let data = std::fs::read_to_string(format!("data/aoc_2025_{arg}"));
-        seq!(N in 1..=12 {
-            match arg {
-            #(
-            stringify!(day~N) => {
-                match data {
-                    Ok(data) => {
-                        match crate::puzzles::day~N::solve(&data) {
-                        Ok((s1, s2)) => {
-                            println!("Part 1 solution: {s1}");
-                            println!("Part 2 solution: {s2}");
-                        }
-                        Err(e) => println!("Error solving puzzle: {e}")
+        None
+    }
+}
+
+#[allow(clippy::cognitive_complexity)]
+fn solve_puzzle(arg: &str) {
+    let data = std::fs::read_to_string(format!("data/aoc_2025_{arg}"));
+    seq!(N in 1..=12 {
+        match arg {
+        #(
+        stringify!(day~N) => {
+            match data {
+                Ok(data) => {
+                    match crate::puzzles::day~N::solve(&data) {
+                    Ok((s1, s2)) => {
+                        println!("Part 1 solution: {s1}");
+                        println!("Part 2 solution: {s2}");
                     }
-                    }
-                    Err(err) => println!("Error reading data 1: {err}"),
+                    Err(e) => println!("Error solving puzzle: {e}")
                 }
+                }
+                Err(err) => println!("Error reading data 1: {err}"),
             }
-            )*
-                _ => panic!("Invalid argument {arg}")
-            }
-        })
+        }
+        )*
+            _ => panic!("Invalid argument {arg}")
+        }
+    })
+}
+
+fn main() {
+    if let Some(arg) = get_puzzle_arg() {
+        solve_puzzle(&arg);
+    } else {
+        println!("No arguments provided");
     }
 }
