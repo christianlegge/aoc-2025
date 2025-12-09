ed17cdb7d8f9d52035fa0ff950175a775368e4c4
Christian Legge <christian@christianlegge.dev>
2025-12-09 10:19:09 -0500

    day 7 pt 2

--- src/puzzles/day7.rs
+++ src/puzzles/day7.rs
@@ -8,7 +8,7 @@ struct Teleporter {
     beams: Vec<HashSet<usize>>,
     splitters: Vec<HashSet<usize>>,
     split_counts: usize,
-    timelines: usize,
+    timelines: Vec<Vec<usize>>,
 }
 
 impl FromStr for Teleporter {
@@ -16,11 +16,14 @@ impl FromStr for Teleporter {
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let mut splitters = Vec::new();
+        let mut timelines: Vec<Vec<usize>> = Vec::new();
         let mut start = 0;
         for line in s.lines() {
             splitters.push(line.match_indices('^').map(|(idx, _)| idx).collect());
+            timelines.push(line.chars().map(|_| 0).collect());
             if let Some(start_idx) = line.find('S') {
                 start = start_idx;
+                timelines[0][start_idx] = 1;
             }
         }
         Ok(Self {
@@ -28,7 +31,7 @@ impl FromStr for Teleporter {
             beams: Vec::new(),
             splitters,
             split_counts: 0,
-            timelines: 0,
+            timelines,
         })
     }
 }
@@ -41,7 +44,7 @@ impl Teleporter {
         let mut split_iter = self.splitters.iter();
         split_iter.next();
 
-        for splits in split_iter {
+        for (idx, splits) in split_iter.enumerate() {
             let last_beams = self.beams.last().expect("guaranteed not empty");
             let mut new_beams = HashSet::new();
             for b in last_beams {
@@ -49,10 +52,14 @@ impl Teleporter {
                     new_beams.insert(b - 1);
                     new_beams.insert(b + 1);
                     self.split_counts += 1;
-                    self.timelines += 2;
                 } else {
                     new_beams.insert(*b);
+                    self.timelines[idx + 1][*b] += self.timelines[idx][*b];
                 }
+            }
+            for s in splits {
+                self.timelines[idx + 1][s - 1] += self.timelines[idx][*s];
+                self.timelines[idx + 1][s + 1] += self.timelines[idx][*s];
             }
             let _new_beam_count = new_beams.iter().filter(|b| !last_beams.contains(b)).count();
             self.beams.push(new_beams);
@@ -62,27 +69,16 @@ impl Teleporter {
 
 #[allow(clippy::unnecessary_wraps)]
 pub fn solve(data: &str) -> Result<(String, String), Error> {
-    let data = r".......S.......
-...............
-.......^.......
-...............
-......^.^......
-...............
-.....^.^.^.....
-...............
-....^.^...^....
-...............
-...^.^...^.^...
-...............
-..^...^.....^..
-...............
-.^.^.^.^.^...^.
-...............
-";
     let mut teleporter = Teleporter::from_str(data)?;
     teleporter.fire_beam();
     Ok((
         teleporter.split_counts.to_string(),
-        teleporter.timelines.to_string(),
+        teleporter
+            .timelines
+            .last()
+            .expect("never empty")
+            .iter()
+            .sum::<usize>()
+            .to_string(),
     ))
 }
