9770f7b8a08de665d150e0c7d13aa5c3213a3c9b
Christian Legge <christian@christianlegge.dev>
2025-12-09 11:01:21 -0500

    day 8 wip

--- src/puzzles/day8.rs
+++ src/puzzles/day8.rs
@@ -1,33 +1,88 @@
-use std::rc::Rc;
+use std::str::FromStr;
 
 use anyhow::Error;
-use num::{abs, integer::sqrt};
+use itertools::Itertools;
+
+#[derive(Debug)]
+struct BoxNetwork {
+    boxes: Vec<JunctionBox>,
+    connections: Vec<(usize, usize)>,
+}
 
 #[derive(Debug)]
 struct JunctionBox {
     x: u64,
     y: u64,
     z: u64,
-    connections: Vec<Rc<JunctionBox>>,
+}
+
+impl BoxNetwork {
+    fn find_next_shortest(&self, at_least: f64) -> (f64, (usize, usize)) {
+        let mut min = f64::INFINITY;
+        let mut ret = (0, 0);
+        for (ia, a) in self.boxes.iter().enumerate() {
+            for (ib, b) in self.boxes.iter().enumerate() {
+                if ia == ib {
+                    continue;
+                }
+                let dist = a.dist(b);
+                if dist < min && dist > at_least {
+                    ret = (ia, ib);
+                    min = dist;
+                }
+            }
+        }
+        (min, ret)
+    }
+
+    fn make_shortest_connections(&mut self, num: usize) {
+        let mut longest_made = 0.0;
+        for _ in 0..num {
+            let (dist, (a, b)) = self.find_next_shortest(longest_made);
+            longest_made = dist;
+            self.connections.push((a, b));
+        }
+    }
+
+    fn print_connections(&self) {
+        println!("connections:");
+        for &(a, b) in &self.connections {
+            println!("{:?}, {:?}", self.boxes[a], self.boxes[b]);
+        }
+    }
 }
 
 impl JunctionBox {
-    fn dist(&self, other: &JunctionBox) -> u64 {
-        sqrt(
-            self.x.abs_diff(other.x).pow(2)
+    fn dist(&self, other: &Self) -> f64 {
+        #[allow(clippy::cast_precision_loss)]
+        f64::sqrt(
+            (self.x.abs_diff(other.x).pow(2)
                 + self.y.abs_diff(other.y).pow(2)
-                + self.z.abs_diff(other.z).pow(2),
+                + self.z.abs_diff(other.z).pow(2)) as f64,
         )
-    }
-
-    fn connect(&mut self, other: &mut JunctionBox) {
-        // other.connections.push(Rc::clone(*self));
-        // self.connections.push(Rc::new(*other));
     }
 }
 
-fn connect(first: Rc<JunctionBox>, second: Rc<JunctionBox>) {
-    // first.connections.push(Rc::clone(&second));
+impl FromStr for JunctionBox {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut parts = s.split(',');
+        Ok(Self {
+            x: parts
+                .next()
+                .ok_or_else(|| anyhow::anyhow!("missing x coord"))?
+                .parse()?,
+            y: parts
+                .next()
+                .ok_or_else(|| anyhow::anyhow!("missing y coord"))?
+                .parse()?,
+            z: parts
+                .next()
+                .ok_or_else(|| anyhow::anyhow!("missing z coord"))?
+                .parse()?,
+        })
+    }
 }
 
 #[allow(clippy::unnecessary_wraps)]
@@ -53,6 +108,19 @@ pub fn solve(data: &str) -> Result<(String, String), Error> {
 984,92,344
 425,690,689
 ";
+
     println!("Text input: {data}");
+    let points: Vec<_> = data.lines().map(JunctionBox::from_str).try_collect()?;
+
+    let mut network = BoxNetwork {
+        boxes: points,
+        connections: Vec::new(),
+    };
+
+    network.make_shortest_connections(10);
+    network.print_connections();
+
+    dbg!(&network);
+
     Ok(("Unimplemented".to_string(), "Unimplemented".to_string()))
 }
